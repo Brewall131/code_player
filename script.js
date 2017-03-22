88232126f54ebe001ddc2f37f335eb1de41e588b
Breana Wallace <breanawallace131@gmail.com>
2017-03-22 19:48:16 -0400

    functionality for html to iframe rendering, correct sizing of the textarea and iframe on page

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ $(document).ready(function (){
 	});
 
 
-	$('textarea').height($(window).height() - $('#header').height()- 10);
+	$('.panel').height($(window).height() - $('#header').height() - 10);
 
+	$('.panel').width(($(window).width() / 2) - 10);
 
+	// FUNCTIONALITY FOR THE HTML TO IFRAME FEATURE //
+	$('textarea').on('change keyup paste', function() {
+
+		$('iframe').contents().find('html').html($('#htmlpanel').val());
+
+	});
 });
